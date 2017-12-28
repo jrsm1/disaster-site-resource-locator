31a8e83e2ded9ee9016184a68575156a2060e9cd
alejandroVasquez812 <alejandro.vasquez@upr.edu>
2017-12-28 13:11:48 -0800

    changed connection url  and added db config

---
+++ config/dbconfig.py
@@ -0,0 +1,8 @@
+#Databse configuration information
+
+pg_config = {
+    'user': 'dsadmin',
+    'passwd': 'databaseds',
+    'host':'dsrl.servebeer.com',
+    'dbname': 'dsDB'
+}
--- dao/PurchaseDAO.py
+++ dao/PurchaseDAO.py
@@ -4,8 +4,9 @@ import psycopg2
 class PurchaseDAO:
     def __init__(self):
 
-        connection_url = "dbname=%s user=%s password=%s" % (pg_config['dbname'],
+        connection_url = "dbname=%s user=%s host=%s password=%s" % (pg_config['dbname'],
                                                             pg_config['user'],
+                                                            pg_config['host'],
                                                             pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
