6fe39dbf253ab3f6ac8be6e461fec2e2f10f9a56
jrsm1 <johnnyrsm1@gmail.com>
2018-01-05 18:38:26 -0400

    Added Clothes DAO/Handler.

---
+++ dao/ClothesDAO.py
@@ -0,0 +1,97 @@
+from config.dbconfig import pg_config
+import psycopg2
+
+class ClothesDAO:
+    def __init__(self):
+
+        connection_url = "dbname=%s user=%s host=%s password=%s" % (pg_config['dbname'],
+                                                            pg_config['user'],
+                                                            pg_config['host'],
+                                                            pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
+
+    def getAllClothes(self):
+        cursor = self.conn.cursor()
+        query = "select * from clothes;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def getClothesById(self, rid):
+        cursor = self.conn.cursor()
+        query = "select * from clothes where rid = %s;"
+        cursor.execute(query, (rid,))
+        result = cursor.fetchone()
+        return result
+
+
+    def getClothesByPrice(self, price):
+        cursor = self.conn.cursor()
+        query = "select * from clothes where price = %s;"
+        cursor.execute(query, (price,))
+        result = [] 
+        for row in cursor:
+             result.append(row)
+        return result
+
+
+    def getClothesByColor(self, color):
+        cursor = self.conn.cursor()
+        query = "select * from clothes where color = %s;"
+        cursor.execute(query, (color,))
+        result = [] 
+        for row in cursor:
+             result.append(row)
+        return result
+
+
+    def getClothesBySize(self, size):
+        cursor = self.conn.cursor()
+        query = "select * from clothes where size = %s;"
+        cursor.execute(query, (size,))
+        result = []                                                                                                   
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def getClothesByGender(self, gender):
+        cursor = self.conn.cursor()
+        query = "select * from clothes where gender = %s;"
+        cursor.execute(query, (gender,))
+        result = []                                                                                                   
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def getClothesByPiece(self, piece):
+        cursor = self.conn.cursor()
+        query = "select * from clothes where piece = %s;"
+        cursor.execute(query, (piece,))
+        result = []                                                                                                   
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def getPurchaseByClothesId(self, rid):
+        cursor = self.conn.cursor()
+        query = "select pid, cid, sid, rid, qty, total, ccnum from purchase natural inner join clothes where rid = %s;"
+        cursor.execute(query, (rid,))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def insert(self, rid, price, color, size, gender, piece):
+        cursor = self.conn.cursor()
+        query = "insert into clothes values (%s, %s, %s, %s, %s, %s) returning rid;"
+        cursor.execute(query, (rid, price, color, size, gender, piece,))
+        rid = cursor.fetchone()[0]
+        self.conn.commit()
+        return rid
---
+++ handlers/ClothesHandler.py
@@ -0,0 +1,173 @@
+from flask import jsonify
+from dao.ClothesDAO import ClothesDAO
+
+
+class ClothesHandler:
+
+    def build_clothes_dict(self, row):
+        result = {}
+        result['rid'] = row[0]
+        result['price'] = row[1]
+        result['color'] = row[2]
+        result['size'] = row[3]
+        result['gender'] = row[4]
+        result['piece'] = row[5]
+        return result
+
+
+    def build_clothes_attributes(self, rid, price, color, size, gender, piece):
+        result = {}
+        result['rid'] = rid
+        result['price'] = price
+        result['color'] = color
+        result['size'] = size
+        result['gender'] = gender
+        result['piece'] = piece
+        return result
+
+
+    def getAllClothes(self):
+
+        dao = ClothesDAO()
+        clothes_list = dao.getAllClothes()
+        result_list = []
+        for row in clothes_list:
+            result = self.build_clothes_dict(row)
+            result_list.append(result)
+        return jsonify(Clothes=result_list)
+
+
+    def getClothesById(self, rid):
+
+        dao = ClothesDAO()
+        row = dao.getClothesById(rid)
+        if not row:
+            return jsonify(Error="Clothes Not Found"), 404
+        else:
+            clothes = self.build_clothes_dict(row)
+        return jsonify(Clothes = clothes)
+
+
+    def getClothesByPrice(self, price):
+
+        dao = ClothesDAO()
+        clothes_list = dao.getClothesByPrice(price)
+        if not clothes_list:
+            return jsonify(Error = "No Clothes found"), 404
+        else:
+            result_list = []
+            for row in clothes_list:
+                result = self.build_clothes_dict(row)
+                result_list.append(result)
+        return jsonify(Clothes = result_list)
+
+
+    def getClothesByColor(self, color):
+
+        dao = ClothesDAO()
+        clothes_list = dao.getClothesByColor(color)
+        if not clothes_list:
+            return jsonify(Error = "No Clothes found"), 404
+        else:
+            result_list = []
+            for row in clothes_list:
+                result = self.build_clothes_dict(row)
+                result_list.append(result)
+        return jsonify(Clothes = result_list)
+
+
+    def getClothesBySize(self, size):
+
+        dao = ClothesDAO()
+        clothes_list = dao.getClothesBySize(size)
+        if not clothes_list:
+            return jsonify(Error = "No Clothes found"), 404
+        else:
+            result_list = []
+            for row in clothes_list:
+                result = self.build_clothes_dict(row)
+                result_list.append(result)
+        return jsonify(Clothes = result_list)
+
+
+    def getClothesByGender(self, gender):
+
+        dao = ClothesDAO()
+        clothes_list = dao.getClothesByGender(gender)
+        if not clothes_list:
+            return jsonify(Error = "No Clothes found"), 404
+        else:
+            result_list = []
+            for row in clothes_list:
+                result = self.build_clothes_dict(row)
+                result_list.append(result)
+        return jsonify(Clothes = result_list)
+
+
+    def getClothesByPiece(self, piece):
+
+        dao = ClothesDAO()
+        clothes_list = dao.getClothesByPiece(piece)
+        if not clothes_list:
+            return jsonify(Error = "No Clothes found"), 404
+        else:
+            result_list = []
+            for row in clothes_list:
+                result = self.build_clothes_dict(row)
+                result_list.append(result)
+        return jsonify(Clothes = result_list)
+
+
+    def searchClothes(self, args):
+        if len(args) > 6:
+            return jsonify(Error = "Malformed search string."), 400
+        else:
+            rid = args.get("rid")
+            price = args.get("price")
+            color = args.get("color")
+            size = args.get("size")
+            gender = args.get("gender")
+            piece = args.get("piece")
+
+            dao = ClothesDAO()
+            clothes_list = []
+            if (len(args) == 1) and rid:
+                clothes_list = dao.getClothesById(rid)
+            elif (len(args) == 1) and price:
+                clothes_list = dao.getClothesByPrice(price)
+            elif (len(args) == 1) and color:
+                clothes_list = dao.getClothesByColor(color)
+            elif (len(args) == 1) and size:
+                clothes_list = dao.getClothesBySize(size)
+            elif (len(args) == 1) and gender:
+                clothes_list = dao.getClothesByGender(gender)
+            elif (len(args) == 1) and piece:
+                clothes_list = dao.getClothesByPiece(piece)
+            else:
+                return jsonify(Error = "Malformed query string"), 400
+            result_list = []
+            for row in clothes_list:
+                result = self.build_clothes_dict(row)
+                result_list.append(result)
+            return jsonify(Clothes = result_list)
+
+
+    def insertClothes(self, form):
+        if len(form) != 6:
+            return jsonify(Error = "Malformed POST request"), 400
+        else:
+            rid = form['rid']
+            price = form['price']
+            color = form['color']
+            size = form['size']
+            gender = form['gender']
+            piece = form['piece']
+
+            if rid and price and color and size and gender and piece:
+                dao = ClothesDAO()
+                dao.insert(rid, price, color, size, gender, piece)
+                result = self.build_clothes_attributes(rid, price, color, size, gender, piece)
+                return jsonify(Clothes = result), 201
+            else:
+                return jsonify(Error="Unexpected attributes in POST request"), 400
+
